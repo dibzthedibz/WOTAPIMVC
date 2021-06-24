2056dbf8ef364181f37c21722f91319eb0fe2d54
Ryan <Dibzthedibz@gmail.com>
2021-06-24 12:04:50 -0700

    Initial commit. Book created but not migrated yet.

---
+++ WOTAPI.Data/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WOTAPI.Data
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public int PageCount { get; set; }        
+    }
+}
---
+++ WOTAPI.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WOTAPI.WebMVC.MvcApplication" Language="C#" %>
